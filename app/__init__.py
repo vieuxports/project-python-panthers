4d7d71ffe043d1024f09149e647d96e54cdfb538
jules <mail.xu2012@gmail.com>
2022-06-03 14:26:11 -0400

    [jul-travel] add path for new page jul-travel

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('/main/index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/jul-travel')
+def julTravel():
+    return render_template('/juliette/jul-travel.html', title="MLH Fellow", url=os.getenv("URL"))
