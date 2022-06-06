24950e9a66557bbdbae9f32cd8e0f15f99999e90
jules <mail.xu2012@gmail.com>
2022-06-05 20:07:01 -0400

    add page route for maansi and josh travels

--- app/__init__.py
+++ app/__init__.py
@@ -13,3 +13,11 @@ def index():
 @app.route('/jul-travel')
 def julTravel():
     return render_template('/juliette/jul-travel.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/maansi-travel')
+def manTravel():
+    return render_template('/maansi/maansi-travel.html', url=os.getenv("URL"))
+
+@app.route('/josh-travel')
+def joshTravel():
+    return render_template('/joshua/josh-travel.html', url=os.getenv("URL"))
