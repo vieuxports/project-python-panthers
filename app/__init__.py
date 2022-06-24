b6aa5477c5e013829b75a3132f503bca16a1913d
jules <mail.xu2012@gmail.com>
2022-06-24 05:08:40 +0000

    database challenge 1

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
+from peewee import *
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
+
+mydb=
+MySQLDatabase(os.getenv("MYSQL_DATABASE"),user=os.getenv("MYSQL_USER"),password=os.getenv("MYSQL_PASSWORD"),host=os.getenv("MYSQL_HOST"),port=3306)
+print(mydb)
 
 @app.route('/')
 def index():
