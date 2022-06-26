3bb8b00b8e71136da428acc8ea9f5b6b890c29b9
jules <mail.xu2012@gmail.com>
2022-06-26 05:17:48 +0000

    update redeploy-site script to use service instead of tmux

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-pkill -f tmux 
-
 cd project-python-panthers
 
 /usr/bin/git fetch && /usr/bin/git reset origin/main --hard
@@ -9,4 +7,4 @@ source python3-virtualenv/bin/activate
 
 pip install -r requirements.txt
 
-tmux new-session -d "source python3-virtualenv/bin/activate && flask run --host=0.0.0.0"
+systemctl restart myportfolio
