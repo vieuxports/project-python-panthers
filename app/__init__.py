b24bc2bc216660b6b9bb98991ec064e2221c866e
xuo <61861557+xuo-lynn@users.noreply.github.com>
2022-07-08 01:50:41 -0400

    pull request for testing

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,31 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+<<<<<<< Updated upstream
+=======
+mydb= MySQLDatabase(os.getenv("MYSQL_DATABASE"),user=os.getenv("MYSQL_USER"),password=os.getenv("MYSQL_PASSWORD"),host=os.getenv("MYSQL_HOST"),port=3306)
+print(mydb)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+if os.getenv("TESTING") == "true":
+    print("Running in   test mode")
+    mydb=SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+
+else:
+    mydb=MySQLDatabase(os.getenv("MYSQL_DATABASE"),user=os.getenv("MYSQL_USER"),password=os.getenv("MYSQL_PASSWORD"),host=os.getenv("MYSQL_HOST"),port=3306)
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
+>>>>>>> Stashed changes
 @app.route('/')
 def index():
     return render_template('/main/index.html', title="Python Panthers Portfolio", url=os.getenv("URL"))
